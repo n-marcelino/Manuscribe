2e78cf6aa867bc9eb24ccc3b2971aa66cad7fa89
n-marcelino <nicomarcelino0613@gmail.com>
2024-03-12 19:07:21 +0800

    fixed navigation

--- app/src/main/java/com/ciit/manuscribe/booklist.kt
+++ app/src/main/java/com/ciit/manuscribe/booklist.kt
@@ -1,11 +1,35 @@
 package com.ciit.manuscribe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class booklist : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booklist)
+
+        val logo = findViewById<ImageView>(R.id.logo)
+        val header = findViewById<TextView>(R.id.header)
+        logo.setOnClickListener() {
+            startActivity(
+                Intent(
+                    applicationContext,
+                    MainActivity::class.java
+                )
+            )
+            finish()
+        }
+        header.setOnClickListener() {
+            startActivity(
+                Intent(
+                    applicationContext,
+                    MainActivity::class.java
+                )
+            )
+            finish()
+        }
     }
 }
