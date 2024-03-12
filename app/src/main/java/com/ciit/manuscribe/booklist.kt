a1f2a67669782ebac93c10f5e56b0bc411b0159b
Kiiyonn <villegaskeon22@gmail.com>
2024-03-12 18:57:38 +0800

    test

---
+++ app/src/main/java/com/ciit/manuscribe/booklist.kt
@@ -0,0 +1,11 @@
+package com.ciit.manuscribe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class booklist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booklist)
+    }
+}
