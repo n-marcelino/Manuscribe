d82e1ce781d348eaade3d37872d189e1f2608775
Kiiyonn <villegaskeon22@gmail.com>
2024-03-13 18:23:57 +0800

    with liked books and nav

--- app/src/main/java/com/ciit/manuscribe/booklist.kt
+++ app/src/main/java/com/ciit/manuscribe/booklist.kt
@@ -1,11 +1,51 @@
 package com.ciit.manuscribe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class booklist : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booklist)
+
+
+        val logo = findViewById<ImageView>(R.id.logo)
+        val header = findViewById<TextView>(R.id.header)
+        logo.setOnClickListener() {
+            startActivity(
+                Intent(
+                    applicationContext,
+                    MainActivity::class.java
+                )
+            )
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+            finish()
+        }
+        header.setOnClickListener() {
+            startActivity(
+                Intent(
+                    applicationContext,
+                    MainActivity::class.java
+                )
+            )
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+            finish()
+
+        }
+//        val image = findViewById<ImageView>(R.id.bookCover)
+//        image.setOnClickListener() {
+//            startActivity(
+//                Intent(
+//                    applicationContext,
+//                    page::class.java
+//                )
+//            )
+//            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+//            finish()
+//        }
+
     }
 }
