579f3e2277085116c75e824750036ac07e01ecb4
n-marcelino <nicomarcelino0613@gmail.com>
2024-03-13 22:44:07 +0800

    changes to bind_step3, added spinner options and fixed multiline text

--- app/src/main/java/com/ciit/manuscribe/bind_step3.kt
+++ app/src/main/java/com/ciit/manuscribe/bind_step3.kt
@@ -3,13 +3,22 @@ package com.ciit.manuscribe
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.LinearLayout
 
 class bind_step3 : AppCompatActivity() {
+
+    var paymentoptions_array = arrayOf("COD", "Credit/Debit Card", "E-Wallet (G-Cash/Maya)", "Bank Transfer")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bind_step3)
+
+        val paymentoptions = findViewById<AutoCompleteTextView>(R.id.paymentoptions)
+        val adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, paymentoptions_array)
+        paymentoptions.setAdapter(adapter)
 
         val goback = findViewById<LinearLayout>(R.id.goback)
         goback.setOnClickListener() {
