d266a066039371c600f6a264c0b6afd7bbfbac55
n-marcelino <nicomarcelino0613@gmail.com>
2024-03-17 00:57:09 +0800

    adding dropdown

--- app/src/main/java/com/ciit/manuscribe/MainActivity.kt
+++ app/src/main/java/com/ciit/manuscribe/MainActivity.kt
@@ -3,7 +3,6 @@ package com.ciit.manuscribe
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
